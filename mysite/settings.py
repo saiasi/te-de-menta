ff60124bcfefb91f809a6ce3019942bc8d697805
Vania <vania.tica@gmail.com>
2015-05-23 16:45:36 -0700

    Remove dj_databas_url configurations from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,9 +102,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
