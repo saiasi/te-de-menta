ad612f9532df8205c239d80e01d3aae5c68a6a25
Vania <vania.tica@gmail.com>
2015-05-23 15:45:11 -0700

    Ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEBUG = False
 
